87c9ee4bc9f1a8d925795b10313ab7b1c9b86938
Benjamin Florin <benjamin.florin@gmail.com>
2017-11-27 18:48:42 -0500

    Added BC footer. Fixes #5

--- common/footer.php
+++ common/footer.php
@@ -1,14 +1,35 @@
       </div>
     </main>
-    <footer role="contentinfo">
-        <hr>
-        <div class="container">
-            <p class="text-center">
-                <?php echo __('Copyright &copy; ') . date('Y') . ' ' . link_to_home_page() . ', All Rights Reserved.'; ?><br>
-                <?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?>
-            </p>
-        </div>
+      <!--<?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?>-->
+
+      <footer role="contentinfo">
+
+      <h3 class="sr-only sr-only-focusable">Boston College footer</h3>
+      <div id="bc-footer">
+          <div class="container">
+              <div class="row">
+                  <div class="bc-img col-md-3 col-sm-12 col-xs-12">
+                      <a href="https://www.bc.edu/content/bc-web.html"><img src="https://www.bc.edu/etc/designs/bc-web/images/logo-footer.png" class="footer-logo" alt="Boston College"></a>
+                  </div>
+                  <div class="col-md-9 col-sm-12 col-xs-12">
+                      <nav class="bc-links">
+                          <ul>
+                              <li><a href="https://www.bc.edu/sites/accessibility.html">Accessibility</a></li>
+                              <li><a href="https://www.bc.edu/bc-web/resources/emergency-r.html">Emergency</a></li>
+                              <li><a href="https://www.bc.edu/bc-web/about/maps-and-directions.html">Maps</a></li>
+                              <li><a href="https://www.bc.edu/bc-web/resources/directories-r.html">Directories</a></li>
+                              <li><a href="https://www.bc.edu/a-z/directories/contact.html">Contact</a></li>
+                          </ul>
+                      </nav>
+                      <p class="copyright">Copyright &copy; 2017 Trustees of Boston College</p>
+                  </div>
+              </div>
+          </div>
+      </div>
+
         <?php fire_plugin_hook('public_footer', ['view' => $this]); ?>
     </footer>
-</body>
+
+      </body>
 </html>
+

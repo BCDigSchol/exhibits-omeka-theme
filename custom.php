cace69591d063dcf7f8040a79dda55c9eb170246
Benjamin Florin <benjamin.florin@gmail.com>
2017-11-27 16:37:13 -0500

    Modluarize header and fix nav background. Fixes #2

--- custom.php
+++ custom.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/functions.php';
 require_once __DIR__ . '/exhibit-builder/helpers.php';
 require_once __DIR__ . '/lib/ExhibitList.php';
+require_once __DIR__ . '/lib/NavMenu.php';
 
 /**
  * Return the markup for the exhibit page navigation.

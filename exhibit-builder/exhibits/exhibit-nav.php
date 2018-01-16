9f6b27f8103477f7afacc313569a4f45df58eac3
Benjamin Florin <benjamin.florin@gmail.com>
2018-01-16 16:37:13 -0500

    Change aria-controls value for exhibit nav button. Fixes #31

--- exhibit-builder/exhibits/exhibit-nav.php
+++ exhibit-builder/exhibits/exhibit-nav.php
@@ -1,6 +1,6 @@
 <div class="exhibit-nav-container">
     <p>
-        <button class="btn btn-primary jump-button collapsed" type="button" data-toggle="collapse" data-target="#exhibit-nav-box" aria-expanded="false" aria-controls="exihibit-nav-box">
+        <button class="btn btn-primary jump-button collapsed" type="button" data-toggle="collapse" data-target="#exhibit-nav-box" aria-expanded="false" aria-controls="exhibit-nav-box">
             Jump to…
         </button>
         <span class="x-button" onclick="$('#exhibit-nav-box').collapse('hide'); console.log('called');">x</span>
